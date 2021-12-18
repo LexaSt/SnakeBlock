98806f989db385a4c69ca64b99abdb6306ada3e8
LexaSt <astajarin@mail.ru>
2021-12-18 18:41:38 +0300

    next step
    поправил платформу и считаем разницу между где голова была и где оказалась

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -5,39 +5,73 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject prefab;
-    private List<Vector3> tails=new List<Vector3>();
-    public Vector3 position;
-    public Vector3 firstPositionTail;
-    public GameObject tail;
-
+    private List<Vector3> tails = new List<Vector3>();
+    private Vector3 position;
+    private Vector3 firstPositionTail;
+    private GameObject tail;
+    public float speed;
+    public Rigidbody HeadSnake;
+    public float sens;
+    public float howMuchTailsX2;
 
 
     void Start()
     {
         position = transform.position;
-        firstPositionTail = new Vector3(position.x, position.y, position.z-0.5f);
-        print(position);
-        print(firstPositionTail);
-        GetNewPrefab(); 
+        firstPositionTail = new Vector3(position.x, position.y, position.z - 0.5f);
+        //print(position);
+        //print(firstPositionTail);
+        GetNewPrefab();
     }
 
 
     void GetNewPrefab()
     {
-        for (float i = 0; i < 2; i=i+0.5f)
+        for (float i = 0; i < howMuchTailsX2; i = i + 0.5f)
         {
-            tail = new GameObject();
             tail = Instantiate(prefab);
-            tail.transform.position = new Vector3 (firstPositionTail.x, firstPositionTail.y, firstPositionTail.z - i);
-            print("Позиция префаба" + tail.transform.position);
+            tail.transform.position = new Vector3(firstPositionTail.x, firstPositionTail.y, firstPositionTail.z - i);
+            //print("Позиция префаба" + tail.transform.position);
             tails.Add(tail.transform.position);
         }
     }
+
+    void movingTails()
+    {
+        Vector3 newPos = HeadSnake.position - position;
+        print("Разница между двумя позициями " + newPos);
+       
     
+    }
+
+    private void control()
+    {
+        if (Input.GetKey("a"))
+        {
+            HeadSnake.velocity = new Vector3(-speed, 0, 0);
+        }
+
+        else if (Input.GetKey("d"))
+        {
+
+            HeadSnake.velocity = new Vector3(speed, 0, 0);
+        }
+        else if (Input.GetKey("w"))
+        {
+            HeadSnake.velocity = new Vector3(0, 0, speed);
+        }
+        else if (Input.GetKey("s"))
+        {
+            HeadSnake.velocity = new Vector3(0, 0, -speed);
+        }
+    }
+
 
     private void Update()
     {
         
-       
+        control();
+        movingTails();
     }
 }
+ 
