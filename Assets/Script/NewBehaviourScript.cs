f4eea9e362075fba9fa0ef5bdb273a0306968727
LexaSt <astajarin@mail.ru>
2021-12-18 15:14:34 +0300

    mov

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -7,8 +7,10 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject prefab;
     private List<Vector3> tails=new List<Vector3>();
     public Vector3 position;
-    
-   
+    public Vector3 firstPositionTail;
+    public GameObject tail;
+
+
 
     void Start()
     {
@@ -18,25 +20,30 @@ public class NewBehaviourScript : MonoBehaviour
 
     void GetNewPrefab()
     {
-        position = transform.position;
-        Vector3 firstPositionTail = new Vector3 (position.x + 1, position.y, position.z + 1);
-        print(position);
-        print(firstPositionTail);
         
         for (int i = 0; i < 2; i++)
         {
-            GameObject tail = new GameObject();
+            tail = new GameObject();
             tail = Instantiate(prefab);
-            tail.transform.position = new Vector3 (firstPositionTail.x + i, firstPositionTail.y, firstPositionTail.z+i);
+           // tail.transform.position = new Vector3 (firstPositionTail.x + i, firstPositionTail.y, firstPositionTail.z+i);
             print("Позиция префаба" + tail.transform.position);
             tails.Add(tail.transform.position);
         }
     }
-
     
 
     private void Update()
     {
-        
+        position = transform.position;
+        firstPositionTail = new Vector3(position.x + 1, position.y, position.z + 1);
+        print(position);
+        print(firstPositionTail);
+
+        for (int i = 0; i < 2; i++)
+        {
+            tail.transform.position = new Vector3(firstPositionTail.x + i, firstPositionTail.y, firstPositionTail.z + i);
+            print("Позиция префаба" + tail.transform.position);
+         
+        }
     }
 }
